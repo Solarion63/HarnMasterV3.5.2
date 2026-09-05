8f10d7ec07ef54a5728ed1f763bcdf28a324b1a1
JB <63668065+Solarion63@users.noreply.github.com>
2026-09-05 15:25:54 -0500

    build: migrate Gulp pipeline to Dart Sass and ESM

---
+++ gulpfile.mjs
@@ -0,0 +1,32 @@
+import gulp from "gulp";
+import autoprefixer from "gulp-autoprefixer";
+import gulpSass from "gulp-sass";
+import dartSass from "sass";
+
+const sass = gulpSass(dartSass);
+const SYSTEM_SCSS = ["scss/**/*.scss"];
+
+/**
+ * Compile HM3 SCSS with Dart Sass and apply browser prefixes.
+ *
+ * Build failures are allowed to fail the task so CI and local builds do not
+ * silently accept invalid stylesheets.
+ *
+ * @returns {NodeJS.ReadWriteStream}
+ */
+export function compileScss() {
+  return gulp
+    .src(SYSTEM_SCSS)
+    .pipe(sass({ style: "expanded" }).on("error", sass.logError))
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gulp.dest("./css"));
+}
+
+/** Watch SCSS sources and rebuild CSS when they change. */
+export function watchScss() {
+  return gulp.watch(SYSTEM_SCSS, compileScss);
+}
+
+export const css = gulp.series(compileScss);
+export const watch = gulp.series(compileScss, watchScss);
+export default watch;
